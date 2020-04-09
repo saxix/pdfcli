e409388b648680517b91d0cbfa9efcd581f1a2b0
sax <s.apostolico@gmail.com>
2020-04-09 22:11:24 +0200

    add --version option

--- src/pdf_cli/main.py
+++ src/pdf_cli/main.py
@@ -1,8 +1,14 @@
 import click
 
-@click.group()
-def main():
-    pass
+
+@click.group(invoke_without_command=True)
+@click.option('--version', is_flag=True, default=False)
+def main(version):
+    if version:
+        import pkg_resources
+        VERSION = pkg_resources.require("pdf_cli")[0].version
+        click.echo(VERSION)
+
 
 from . import commands
 
